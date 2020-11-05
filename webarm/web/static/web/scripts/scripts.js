9027ad630e50cbd35a8432d12094ca06f2813409
Aleksandr <golovasanya@mail.ru>
2020-11-05 19:02:42 +0200

    changed api url in js scripts

--- webarm/web/static/web/scripts/scripts.js
+++ webarm/web/static/web/scripts/scripts.js
@@ -4,10 +4,10 @@ var app = angular.module('Playground', []);
 app.controller('PlaygroundControll', function($scope, $http, $interval) {
 
   requestData = function() {
-    $http.get("http://localhost:8000/api/v1/device").then(function (response) {
+    $http.get("http://bfcloud.space/api/v1/device").then(function (response) {
       $scope.DeviceData = response.data;
     });
-    $http.get("http://localhost:8000/api/v1/device/current-values").then(function (response) {
+    $http.get("http://bfcloud.space/api/v1/device/current-values").then(function (response) {
       $scope.Tags = response.data;
     });
   };
