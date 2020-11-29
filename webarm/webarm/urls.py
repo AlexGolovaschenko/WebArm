dafed909b8e5164ad2e7bb47fe2c60eeef245a99
Aleksandr <golovasanya@mail.ru>
2020-11-29 12:44:15 +0200

    test

--- webarm/webarm/urls.py
+++ webarm/webarm/urls.py
@@ -7,7 +7,7 @@ from devices import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('device/', include('devices.urls')),
-    path('api/v1/device/', include('devices.urls')),
+    path('api/v1/', include('devices.urls')), #TODO: delte this row
     path('company/', include('companies.urls')),
     path('user/', include('users.urls')),
 
